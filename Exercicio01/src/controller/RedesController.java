cd35d0a88bf29e1d1a4aa111458afb0659745338
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-09-08 16:17:53 -0300

    exercicio 01 finalizado

--- Exercicio01/src/controller/RedesController.java
+++ Exercicio01/src/controller/RedesController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
+
 public class RedesController {
 	
 	public String os() {
@@ -7,16 +13,98 @@ public class RedesController {
 		return os;
 	}
 
-	public String ip(String so) {
-		String nomeAdaptador = "é isso aeee";
+	public void ip(String so) {
 		
-		return nomeAdaptador;
+		int contAdaptador = 0, contIpv4 = 0, i = 0;
+		String[] adaptador = new String[5];
+		String[] ipv4 = new String[5];
+		String process = "";
+		
+		if(so.contains("Linux")){
+			process = "ifconfig";
+		} else if (so.contains("Windows")) {
+			process = "ipconfig";
+		}
+		
+		try {
+			Process p = Runtime.getRuntime().exec(process);
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			
+			while(linha != null) {
+				if(linha.contains("Adaptador")) {
+					adaptador[contAdaptador] = linha;
+					contAdaptador++;
+				}
+				if(linha.contains("ipv4")) {
+					ipv4[contIpv4] = linha;
+					contIpv4++;
+				}
+				
+				linha = buffer.readLine();
+			}
+			
+			fluxo.close();
+			leitor.close();
+			buffer.close();
+			
+			for(i=0; i<contAdaptador; i++) {
+				JOptionPane.showConfirmDialog(null, adaptador[i]);
+				}
+			
+			for(i=0; i<contIpv4; i++) {
+				JOptionPane.showConfirmDialog(null, ipv4[i]);
+				}
+				
+			} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
-	public double ping(String so) {
+	public void ping(String so) {
 		double media = 0;
+		String process = "";
+		int contador = 0;
 		
-		return media;
+		if(so.contains("Linux")){
+			process = "ping www.google.com";
+		} else if (so.contains("Windows")) {
+			process = "ping www.google.com";
+		}
+		
+		try {
+			Process p = Runtime.getRuntime().exec(process);
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			
+			while(linha != null & contador < 10) {
+				
+				if(linha.contains("time") || linha.contains("tempo")) {
+					int index = linha.indexOf("ms");
+					String ms = linha.substring(index-5, index);
+					double x = Double.parseDouble(ms);
+					media = media + x;
+					contador++;				
+				}
+				
+				linha = buffer.readLine();
+			}
+			
+			fluxo.close();
+			leitor.close();
+			buffer.close();
+			
+			JOptionPane.showConfirmDialog(null, "Média: " + media/10);
+				
+			} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
--- Exercicio01/src/view/Main.java
+++ Exercicio01/src/view/Main.java
@@ -8,22 +8,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Object[] opcoes = {01, 02};
+		Object[] opcoes = {1, 2, 9};
 		
 		RedesController operacoes = new RedesController();
 		
-		int selecionado = (int) JOptionPane.showInputDialog(null, "Escolha um item: \n1- Chamada de config do IP\n2 - Calculo do tempo médio do PING: ", "Exercício 01", JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+		int selecionado = (int) JOptionPane.showInputDialog(null, "Escolha um item: \n1- Chamada de config do IP\n2 - Calculo do tempo médio do PING:\n9- Encerrar programa", "Exercício 01", JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
 		
 		switch(selecionado) {
-		case 1:
-			JOptionPane.showMessageDialog(null, operacoes.ip(operacoes.os()));
+		
+		case 1: operacoes.ip(operacoes.os());
 			break;
-		case 2:
-			JOptionPane.showMessageDialog(null, operacoes.ping(operacoes.os()));
+			
+		case 2: operacoes.ping(operacoes.os());
 			break;
-		case 0:
+			
+		case 9:
+			JOptionPane.showMessageDialog(null, "Programa finalizado");
 			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Opção inválida");
 		}
+			
+		
 	}
 
 }
